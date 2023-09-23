6de81078ad8c0aaffe18f6f2eb494328c49752ed
JesserHamdaoui <107654958+JesserHamdaoui@users.noreply.github.com>
2023-09-23 14:24:05 +0100

    CREATE COMPONENTS
    Add TimerContainer component which contain MainTimer BreakTimer and Timer

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import TimerContainer from "./components/TimerContainer";
 
 const App = () => {
-  return <div></div>;
+  return (
+    <div id="app">
+      <TimerContainer />
+    </div>
+  );
 };
 
 export default App;
